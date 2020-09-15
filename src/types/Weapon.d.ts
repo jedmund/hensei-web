a0a40c54b845d27e3b2fd0496a80a7339e85e858
Justin Edmund <justin@jedmund.com>
2020-09-15 04:15:07 -0700

    Add Weapon type

---
+++ src/types/Weapon.d.ts
@@ -0,0 +1,28 @@
+interface Weapon {
+    id: string
+    granblue_id: number
+    element: number
+    proficiency: number
+    max_level: number
+    max_skill_level: number
+    name: { 
+        en: string
+        jp: string
+    }
+    hp: {
+        min_hp: number
+        max_hp: number
+        max_hp_flb: number
+        max_hp_ulb: number
+    }
+    atk: {
+        min_atk: number
+        max_atk: number
+        max_atk_flb: number
+        max_atk_ulb: number
+    }
+    uncap: {
+        flb: boolean
+        ulb: boolean
+    }
+}
