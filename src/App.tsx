fbf3ab02138f595b7082f9b69980018c952acf7e
Justin Edmund <justin@jedmund.com>
2020-09-15 01:25:20 -0700

    Clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,7 @@ import Header from './components/Header/Header'
 import Main from './Main/Main'
 
 function App() {
-    const [parties, setParties] = useState(false)
-    
-    useEffect(() => {
-        getParty()
-    }, [])
-
-    function getParty() {
-        fetch('http://localhost:3001/parties')
-            .then(response => {
-                return response.text()
-            })
-            .then(data => {
-                setParties((data != null) ? true : false)
-            })
-    }
-
-    function createParty() {
-        fetch('http://localhost:3001/parties', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-            // body: JSON.stringify({stuff})
-        })
-            .then(response => {
-                return response.text()
-            })
-            .then(data => {
-                alert(data)
-                getParty()
-            })
-    }
-
-    function deleteParty() {
-        let hash = prompt('Enter hash')
-        fetch(`http://localhost:3001/parties/${hash}`, {
-            method: 'DELETE'
-        })
-            .then(response => {
-                return response.text()
-            })
-            .then(data => {
-                alert(data)
-                getParty()
-            })
-    }
-
-    return (
+    return (   
         <div>
             <Header />
             <Main />
