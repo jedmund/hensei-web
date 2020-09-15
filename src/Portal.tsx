d723b0bc1dea73454930d4c273f49f10285fb616
Justin Edmund <justin@jedmund.com>
2020-09-14 18:29:45 -0700

    Add Portal file

---
+++ src/Portal.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { createPortal } from 'react-dom'
+
+const Portal = ({ children }) => {
+    let modalRoot = document.getElementById('modal')
+
+    if (!modalRoot) {
+        modalRoot = document.createElement('div')
+        modalRoot.setAttribute('id', 'modal')
+        document.body.appendChild(modalRoot)
+    }
+
+    const modalElement = document.createElement('div')
+    
+    useEffect(() => {
+        modalRoot.appendChild(modalElement)
+        return () => modalRoot.removeChild(modalElement)
+    })
+
+    return createPortal(children, modalElement)
+}
+
+export default Portal
