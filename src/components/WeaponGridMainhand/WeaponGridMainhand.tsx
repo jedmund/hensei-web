84b0934e0a5c1cdab0081b1c7d6d89bcc3612fe1
Justin Edmund <justin@jedmund.com>
2020-09-14 18:29:40 -0700

    Add Search modal to weapon cells

--- src/components/WeaponGridMainhand/WeaponGridMainhand.tsx
+++ src/components/WeaponGridMainhand/WeaponGridMainhand.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './WeaponGridMainhand.css'
 
-import Plus from '../../assets/plus.svg'
+import SearchModal from '../SearchModal/SearchModal'
+import { useModal as useModal } from '../../useModal'
 
-const WeaponGridUnit = () => (
-    <div className="WeaponGridMainhand">
-        <span className='icon'><Plus /></span>
-    </div>
-)
+import Plus from '../../../assets/plus.svg'
+
+function WeaponGridUnit() {
+    const { open, openModal, closeModal } = useModal()
+
+    return (
+        <div>
+            <div className="WeaponGridMainhand" onClick={openModal}>
+                <span className='icon'><Plus /></span>
+            </div>
+            {open ? (
+                <SearchModal 
+                    close={closeModal}
+                    placeholderText="Search for a weapon..."
+                />
+            ) : null}
+        </div>
+    )
+}
 
 export default WeaponGridUnit
--- src/components/WeaponGridUnit/WeaponGridUnit.tsx
+++ src/components/WeaponGridUnit/WeaponGridUnit.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+
 import './WeaponGridUnit.css'
 
-import Plus from '../../assets/plus.svg'
+import SearchModal from '../SearchModal/SearchModal'
+import { useModal as useModal } from '../../useModal'
 
-const WeaponGridUnit = () => (
-    <div className="WeaponGridUnit">
-        <span className='icon'><Plus /></span>
-    </div>
-)
+import Plus from '../../../assets/plus.svg'
+
+function WeaponGridUnit() {
+    const { open, openModal, closeModal } = useModal()
+
+    return (
+        <div>
+            <div className="WeaponGridUnit" onClick={openModal}>
+                <span className='icon'><Plus /></span>
+            </div>
+            {open ? (
+                <SearchModal 
+                    close={closeModal}
+                    placeholderText="Search for a weapon..."
+                />
+            ) : null}
+        </div>
+    )
+}
 
 export default WeaponGridUnit
