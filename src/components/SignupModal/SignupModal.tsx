d3d437235f2106916eb37c61990548332a3d7753
Justin Edmund <justin@jedmund.com>
2020-09-18 12:59:59 -0700

    Implement design for Sign up form

--- src/components/SignupModal/SignupModal.tsx
+++ src/components/SignupModal/SignupModal.tsx
@@ -4,18 +4,36 @@ import Portal from '~utils/Portal'
 import Modal from '~components/Modal/Modal'
 import Overlay from '~components/Overlay/Overlay'
 
-const SignupModal = ({ close }) => {
+import './SignupModal.css'
+
+import New from '../../../assets/new'
+
+interface Props {
+    close: () => void
+}
+
+const SignupModal = (props: Props) => {
     return (
         <Portal>
             <div>
-                <Modal>
-                    <input className="Input" name="username" type="text" placeholder="Username" />
-                    <input className="Input" name="email" type="text" placeholder="Email address" />
-                    <input className="Input" name="password" type="password" placeholder="Password" />
-                    <input className="Input" name="confirm_password" type="password" placeholder="Password (again)" />
-                    <button className="Button">Sign up</button>
+                <Modal styleName="SignupForm">
+                    <div id="ModalTop">
+                        <h1>Sign up</h1>
+                        <i className='close' onClick={props.close}><New /></i>
+                    </div>
+                    <form>
+                        <div className="fieldset">
+                            <input className="Input" name="username" type="text" placeholder="Username" />
+                            <input className="Input" name="email" type="text" placeholder="Email address" />
+                            <input className="Input" name="password" type="password" placeholder="Password" />
+                            <input className="Input" name="confirm_password" type="password" placeholder="Password (again)" />
+                        </div>
+                        <div id="ModalBottom">
+                            <button className="Button">Sign up</button>
+                        </div>
+                    </form>
                 </Modal>
-                <Overlay onClick={close} />
+                <Overlay onClick={props.close} />
             </div>
         </Portal>
     )
