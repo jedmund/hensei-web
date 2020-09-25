365de7ceab2aa2dfd7aef2b8be40e183daa19ec7
Justin Edmund <justin@jedmund.com>
2020-09-25 03:39:58 -0700

    Implement cookies

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './components/App/App'
 
 ReactDOM.render((
     <BrowserRouter>
-        <App />
+        <CookiesProvider>
+            <App />
+        </CookiesProvider>
     </BrowserRouter>
 ), document.getElementById('root'))
