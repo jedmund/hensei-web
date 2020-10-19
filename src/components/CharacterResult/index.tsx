320bbf9f3aa3d919ad24be3f0c5589cc28724aa4
Justin Edmund <justin@jedmund.com>
2020-10-19 05:12:08 -0700

    Add CharacterResult

---
+++ src/components/CharacterResult/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import WeaponLabelIcon from '~components/WeaponLabelIcon'
+
+import images from '../../images/chara-grid/*.jpg'
+
+import './index.css'
+
+interface Props {
+    data: Character
+    onClick: () => void
+}
+
+const Element = ['null', 'wind', 'fire', 'water', 'earth', 'dark', 'light']
+
+class CharacterResult extends React.Component<Props> {
+    render() {
+        let imgSrc
+
+        const character = this.props.data
+        if (process.env.NODE_ENV === 'development') {
+            imgSrc = images[character.granblue_id]
+        } else if (process.env.NODE_ENV === 'production') {
+            imgSrc = `${process.env.SIERO_IMG_URL}/chara-grid/${character.granblue_id}.jpg`
+        }
+
+        return (
+            <li className="CharacterResult" onClick={this.props.onClick}>
+                <img alt={character.name.en} src={imgSrc} />
+                <div>
+                    <div>
+                        <h5>{character.name.en}</h5>
+                        <div className="stars">{(character.rarity == 2) ? '⭑⭑⭑' : '⭑⭑⭑⭑'}{(character.uncap.flb) ? <span>⭑</span> : ''}</div>
+                    </div>
+                    <WeaponLabelIcon labelType={Element[character.element]} />
+                </div>
+            </li>
+        )
+    }
+}
+
+export default CharacterResult
