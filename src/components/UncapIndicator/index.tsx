20dc930f5c82a9c1fc3d0e451ce4c9eb0bb7261b
Justin Edmund <justin@jedmund.com>
2020-10-19 05:12:24 -0700

    Update to work with Character's different stars

--- src/components/UncapIndicator/index.tsx
+++ src/components/UncapIndicator/index.tsx
@@ -6,26 +6,38 @@ import './index.css'
 
 
 interface Props {
+    type: 'character' | 'weapon' | 'summon'
+    rarity?: number
     uncapLevel: number
     flb: boolean
-    ulb: boolean
+    ulb?: boolean
 }
 
 const UncapIndicator = (props: Props) => {
     let numStars
-    if (props.ulb) {
-        numStars = 5
-    } else if (props.flb) {
-        numStars = 4
+
+    if (props.type === 'character') {
+        if (props.flb) {
+            numStars = 5
+        } else {
+            numStars = 4
+        }
     } else {
-        numStars = 3
+        if (props.ulb) {
+            numStars = 5
+        } else if (props.flb) {
+            numStars = 4
+        } else {
+            numStars = 3
+        }
     }
 
     return (
         <ul className="UncapIndicator">
             {
                 Array.from(Array(numStars)).map((x, i) => {
-                    if (i > 2) {
+                    if (props.type === 'character' && i > 3 ||
+                        props.type !== 'character' && i > 2) {
                         return <UncapStar uncap={true} key={`star_${i}`} />
                     } else {
                         return <UncapStar uncap={false} key={`star_${i}`} />
