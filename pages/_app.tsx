374e19b0071368c375c78b865bbe45a7bc84fe50
Justin Edmund <justin@jedmund.com>
2022-01-31 20:47:34 -0800

    Add cookies and Layout to index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.scss'
 
 import type { AppProps } from 'next/app'
+import { CookiesProvider } from 'react-cookie'
+import Layout from '~components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <CookiesProvider>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </CookiesProvider>
+    )
 }
 
 export default MyApp
