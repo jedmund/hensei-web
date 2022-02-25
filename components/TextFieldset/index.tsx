f9857eb77233a69d01e673d75f80a66e9f0f61c7
Justin Edmund <justin@jedmund.com>
2022-02-24 18:18:28 -0800

    Add components for textareas and text limited fields

---
+++ components/TextFieldset/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './index.scss'
+
+interface Props {
+    fieldName: string
+    placeholder: string
+    value?: string
+    error: string
+    onBlur?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+    onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+const TextFieldset = React.forwardRef<HTMLTextAreaElement, Props>(function fieldSet(props, ref) {
+    return (
+        <fieldset className="Fieldset">
+            <textarea
+                className="Input" 
+                name={props.fieldName} 
+                placeholder={props.placeholder}
+                defaultValue={props.value || ''}
+                onBlur={props.onBlur}
+                onChange={props.onChange}
+                ref={ref}
+            />
+            {
+                props.error.length > 0 && 
+                <p className='InputError'>{props.error}</p>
+            }
+        </fieldset>
+    )
+})
+
+export default TextFieldset
