ea69c352d5811d06bfdb5c0fc020e9902ec267c7
Justin Edmund <justin@jedmund.com>
2022-03-04 07:08:55 -0800

    Fix Cassius' file name

--- utils/pictureData.tsx
+++ utils/pictureData.tsx
@@ -61,7 +61,7 @@ export const pictureData: Picture[] = [
             en: "Cassius",
             jp: "カシウス"
         },
-        filename: "Cassius",
+        filename: "cassius",
         element: "dark"
     },
     {
