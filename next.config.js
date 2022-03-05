80089843a3ba98d15e5ac3078a8fce5adc22ddda
Justin Edmund <justin@jedmund.com>
2022-03-04 17:55:51 -0800

    Install and setup i18n packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   reactStrictMode: true,
@@ -7,6 +8,7 @@ module.exports = {
     prependData: '@import "variables";',
     includePaths: [path.join(__dirname, 'styles')],
   },
+  i18n,
   async rewrites() {
     return [
       {
