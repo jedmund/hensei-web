b1170a8e878564ba61edf49e9469382aa60861bc
Justin Edmund <justin@jedmund.com>
2022-03-11 01:15:11 -0800

    Add new types, states and value stores

---
+++ types/ElementState.d.ts
@@ -0,0 +1,10 @@
+interface ElementState {
+    [key: string]: CheckedState
+    null: CheckedState
+    wind: CheckedState
+    fire: CheckedState
+    water: CheckedState
+    earth: CheckedState
+    dark: CheckedState
+    light: CheckedState
+}
---
+++ utils/stateValues.tsx
@@ -0,0 +1,16 @@
+export const rarities = ["sr", "ssr"]
+
+export const elements = ["null", "wind", "fire", "water", "earth", "dark", "light"]
+
+export const proficiencies = [
+    "sabre", "dagger", "spear", "axe", "staff",
+    "melee", "gun", "bow", "harp", "katana"
+]
+
+export const weaponSeries = [
+    "seraphic", "grand", "opus", "draconic", "ultima",
+    "bahamut", "omega", "primal", "olden_primal", "militis",
+    "beast", "rose", "xeno", "hollowsky", "astral",
+    "epic", "ennead", "cosmos", "ancestral", "superlative",
+    "vintage", "class_champion", "sephira", "new_world"
+]
