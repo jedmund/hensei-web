b8b6f3231b8b82f0120d7c123bf2007537e6bbc7
Justin Edmund <justin@jedmund.com>
2022-03-11 01:13:54 -0800

    Implement SearchFilterCheckboxItem component

---
+++ components/SearchFilterCheckboxItem/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+
+import CheckIcon from '~public/icons/Check.svg'
+import './index.scss'
+
+interface Props {
+    checked?: boolean
+    valueKey: string
+    onCheckedChange: (open: boolean, key: string) => void
+    children: React.ReactNode
+}
+
+const SearchFilterCheckboxItem = (props: Props) => {
+    function handleCheckedChange(checked: boolean) {
+        props.onCheckedChange(checked, props.valueKey)
+    }
+
+    return (
+        <DropdownMenu.CheckboxItem 
+            className="Item" 
+            checked={props.checked || false}
+            onCheckedChange={handleCheckedChange}
+            onSelect={ (event) => event.preventDefault() }>
+                <DropdownMenu.ItemIndicator className="Indicator">
+                    <CheckIcon />
+                </DropdownMenu.ItemIndicator>
+                {props.children}
+        </DropdownMenu.CheckboxItem>
+    )
+}
+
+export default SearchFilterCheckboxItem
