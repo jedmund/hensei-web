258f9cf89900f57cd61939ffb835eba7dec35e39
Justin Edmund <justin@jedmund.com>
2022-03-14 16:45:49 -0700

    Persist timestamp state in grid API calls

--- components/CharacterGrid/index.tsx
+++ components/CharacterGrid/index.tsx
@@ -86,7 +86,9 @@ const CharacterGrid = (props: Props) => {
         appState.party.id = party.id
         appState.party.user = party.user
         appState.party.favorited = party.favorited
-
+        appState.party.created_at = party.created_at
+        appState.party.updated_at = party.updated_at
+        
         setFound(true)
         setLoading(false)
 
