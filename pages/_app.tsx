84a7ca7733f764a0afee7a5bec5cf03a3b370883
Justin Edmund <justin@jedmund.com>
2022-11-15 04:16:02 -0800

    Update to store authorized state in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,35 @@
+import { useEffect } from "react"
+import { getCookie } from "cookies-next"
 import { appWithTranslation } from "next-i18next"
 
 import type { AppProps } from "next/app"
 import Layout from "~components/Layout"
 
+import { accountState } from "~utils/accountState"
+
 import "../styles/globals.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const cookie = getCookie("account")
+  const cookieData = cookie ? JSON.parse(cookie as string) : null
+
+  useEffect(() => {
+    if (cookie) {
+      console.log(`Logged in as user "${cookieData.username}"`)
+
+      accountState.account.authorized = true
+      accountState.account.user = {
+        id: cookieData.user_id,
+        username: cookieData.username,
+        picture: "",
+        element: "",
+        gender: 0,
+      }
+    } else {
+      console.log(`You are not currently logged in.`)
+    }
+  }, [cookieData])
+
   return (
     <Layout>
       <Component {...pageProps} />
