1f54aa6dbc93327efb719511a133949c416a45b8
Justin Edmund <justin@jedmund.com>
2022-11-16 05:17:32 -0800

    Add Disaster raid group

--- utils/raidGroups.tsx
+++ utils/raidGroups.tsx
@@ -1,124 +1,130 @@
 interface RaidGroup {
-    name: {
-        [key: string]: string
-        en: string
-        ja: string
-    }
+  name: {
+    [key: string]: string
+    en: string
+    ja: string
+  }
 }
 
 export const raidGroups: RaidGroup[] = [
-    {
-        name: {
-            en: 'Assorted',
-            ja: 'その他'
-        }
+  {
+    name: {
+      en: "Assorted",
+      ja: "その他",
     },
-    {
-        name: {
-            en: 'Guild Wars',
-            ja: '星の古戦場'
-        }
+  },
+  {
+    name: {
+      en: "Guild Wars",
+      ja: "星の古戦場",
     },
-    {
-        name: {
-            en: 'Omega',
-            ja: 'マグナ'
-        }
+  },
+  {
+    name: {
+      en: "Omega",
+      ja: "マグナ",
     },
-    {
-        name: {
-            en: 'T1 Summons',
-            ja: '召喚石マルチ1（旧）'
-        }
+  },
+  {
+    name: {
+      en: "T1 Summons",
+      ja: "召喚石マルチ1（旧）",
     },
-    {
-        name: {
-            en: 'T2 Summons',
-            ja: '召喚石マルチ2（新）'
-        }
+  },
+  {
+    name: {
+      en: "T2 Summons",
+      ja: "召喚石マルチ2（新）",
     },
-    {
-        name: {
-            en: 'Primarchs',
-            ja: '四大天使'
-        }
+  },
+  {
+    name: {
+      en: "Primarchs",
+      ja: "四大天使",
     },
-    {
-        name: {
-            en: 'Nightmare',
-            ja: 'HELL'
-        }
+  },
+  {
+    name: {
+      en: "Nightmare",
+      ja: "HELL",
     },
-    {
-        name: {
-            en: 'Omega (Impossible)',
-            ja: 'マグナHL'
-        }
+  },
+  {
+    name: {
+      en: "Omega (Impossible)",
+      ja: "マグナHL",
     },
-    {
-        name: {
-            en: 'Omega II',
-            ja: 'マグナII'
-        }
+  },
+  {
+    name: {
+      en: "Omega II",
+      ja: "マグナII",
     },
-    {
-        name: {
-            en: 'Tier 1 Summons (Impossible)',
-            ja: '旧召喚石HL'
-        }
+  },
+  {
+    name: {
+      en: "Tier 1 Summons (Impossible)",
+      ja: "旧召喚石HL",
     },
-    {
-        name: {
-            en: 'Tier 3 Summons',
-            ja: 'エピックHL'
-        }
+  },
+  {
+    name: {
+      en: "Tier 3 Summons",
+      ja: "エピックHL",
     },
-    {
-        name: {
-            en: 'Ennead',
-            ja: 'エニアド'
-        }
+  },
+  {
+    name: {
+      en: "Ennead",
+      ja: "エニアド",
     },
-    {
-        name: {
-            en: 'Malice',
-            ja: 'マリス'
-        }
+  },
+  {
+    name: {
+      en: "Malice",
+      ja: "マリス",
     },
-    {
-        name: {
-            en: '6-Star Raids',
-            ja: '★★★★★★'
-        }
+  },
+  {
+    name: {
+      en: "6-Star Raids",
+      ja: "★★★★★★",
     },
-    {
-        name: {
-            en: 'Six-Dragons',
-            ja: '六竜HL'
-        }
+  },
+  {
+    name: {
+      en: "Six-Dragons",
+      ja: "六竜HL",
     },
-    {
-        name: {
-            en: 'Nightmare (Impossible)',
-            ja: '高級HELL'
-        }
+  },
+  {
+    name: {
+      en: "Nightmare (Impossible)",
+      ja: "高級HELL",
     },
-    {
-        name: {
-            en: 'Arcarum: Replicard Sandbox',
-            ja: 'アーカルム　レプリカルド・サンドボックス'
-        }
+  },
+  {
+    name: {
+      en: "Arcarum: Replicard Sandbox",
+      ja: "アーカルム　レプリカルド・サンドボックス",
     },
-    {
-        name: {
-            en: 'Astrals',
-            ja: '星の民'
-        }
+  },
+  {
+    name: {
+      en: "Astrals",
+      ja: "星の民",
     },
-    {
-        name: {
-            en: 'Super Ultimate',
-            ja: 'スーパーアルティメット'
-        }
-    }
+  },
+  {
+    name: {
+      en: "Disaster",
+      ja: "災害",
+    },
+  },
+  {
+    name: {
+      en: "Super Ultimate",
+      ja: "スーパーアルティメット",
+    },
+  },
 ]
