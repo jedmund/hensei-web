94c885513d7567c28250f543a767f283d7421f58
Justin Edmund <justin@jedmund.com>
2022-11-16 05:17:46 -0800

    Update spacing

--- types/Party.d.ts
+++ types/Party.d.ts
@@ -1,14 +1,14 @@
 interface Party {
-    id: string
-    name: string
-    raid: Raid
-    shortcode: string
-    extra: boolean
-    favorited: boolean
-    characters: Array<GridCharacter>
-    weapons: Array<GridWeapon>
-    summons: Array<GridSummon>
-    user: User
-    created_at: string
-    updated_at: string
+  id: string
+  name: string
+  raid: Raid
+  shortcode: string
+  extra: boolean
+  favorited: boolean
+  characters: Array<GridCharacter>
+  weapons: Array<GridWeapon>
+  summons: Array<GridSummon>
+  user: User
+  created_at: string
+  updated_at: string
 }
