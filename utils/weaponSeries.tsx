d78545c198cc08dc729ff597419073ebf5f65a8b
Justin Edmund <justin@jedmund.com>
2022-12-24 23:31:13 -0800

    Add utils for weaponSeries and mapping them from IDs

---
+++ utils/weaponSeries.tsx
@@ -0,0 +1,119 @@
+export interface WeaponSeries {
+  id: number
+  slug: string
+}
+
+export const weaponSeries: WeaponSeries[] = [
+  {
+    id: 0,
+    slug: 'seraphic',
+  },
+  {
+    id: 1,
+    slug: 'grand',
+  },
+  {
+    id: 2,
+    slug: 'opus',
+  },
+  {
+    id: 3,
+    slug: 'draconic',
+  },
+  {
+    id: 4,
+    slug: 'revenant',
+  },
+  {
+    id: 6,
+    slug: 'primal',
+  },
+  {
+    id: 7,
+    slug: 'beast',
+  },
+  {
+    id: 8,
+    slug: 'regalia',
+  },
+  {
+    id: 9,
+    slug: 'omega',
+  },
+  {
+    id: 10,
+    slug: 'olden_primal',
+  },
+  {
+    id: 11,
+    slug: 'militis',
+  },
+  {
+    id: 12,
+    slug: 'hollowsky',
+  },
+  {
+    id: 13,
+    slug: 'xeno',
+  },
+  {
+    id: 14,
+    slug: 'astral',
+  },
+  {
+    id: 15,
+    slug: 'rose',
+  },
+  {
+    id: 16,
+    slug: 'bahamut',
+  },
+  {
+    id: 17,
+    slug: 'ultima',
+  },
+  {
+    id: 18,
+    slug: 'epic',
+  },
+  {
+    id: 19,
+    slug: 'ennead',
+  },
+  {
+    id: 20,
+    slug: 'cosmic',
+  },
+  {
+    id: 21,
+    slug: 'ancestral',
+  },
+  {
+    id: 22,
+    slug: 'superlative',
+  },
+  {
+    id: 23,
+    slug: 'vintage',
+  },
+  {
+    id: 24,
+    slug: 'class_champion',
+  },
+  {
+    id: 25,
+    slug: 'proving',
+  },
+  {
+    id: 28,
+    slug: 'sephira',
+  },
+  {
+    id: 29,
+    slug: 'new_world',
+  },
+  {
+    id: 30,
+    slug: 'disaster',
+  },
+]
