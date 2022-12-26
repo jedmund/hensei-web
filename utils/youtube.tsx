bfd73ec044d961bfe396693b70f2d78d053b3413
Justin Edmund <justin@jedmund.com>
2022-12-26 09:51:36 -0800

    I'm dumb

--- utils/youtube.tsx
+++ utils/youtube.tsx
@@ -2,5 +2,5 @@ import { YoutubeAPIClient } from 'youtube-api-v3-wrapper'
 
 export const youtube = new YoutubeAPIClient(
   'key',
-  process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+  process.env.NEXT_PUBLIC_YOUTUBE_API_KEY ? ''
 )
