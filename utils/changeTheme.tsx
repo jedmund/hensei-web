dfc4e5dfd6d7b22bd00d6ed496b8e1bbdede0d1f
Justin Edmund <justin@jedmund.com>
2022-12-25 20:12:07 -0800

    Change theme wasnt necessary

--- utils/changeTheme.tsx
+++
@@ -1,9 +0,0 @@
-import { useTheme } from 'next-themes'
-import { accountState } from '~utils/accountState'
-
-export default function changeTheme() {
-  const { theme, setTheme } = useTheme()
-  if (accountState.account.user) {
-    setTheme(accountState.account.user.theme)
-  }
-}
