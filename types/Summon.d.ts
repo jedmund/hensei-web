05bdb98eb02408517fac52331d24beb6a2f4e8d5
Justin Edmund <justin@jedmund.com>
2023-01-22 19:59:53 -0800

    Add XLB to summon type

--- types/Summon.d.ts
+++ types/Summon.d.ts
@@ -15,16 +15,19 @@ interface Summon {
     max_hp: number
     max_hp_flb: number
     max_hp_ulb: number
+    max_hp_xlb: number
   }
   atk: {
     min_atk: number
     max_atk: number
     max_atk_flb: number
     max_atk_ulb: number
+    max_atk_xlb: number
   }
   uncap: {
     flb: boolean
     ulb: boolean
+    xlb: boolean
   }
   position?: number
 }
