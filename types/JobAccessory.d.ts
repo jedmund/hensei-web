1d1a941364179f5520f0f090f2957a90b5f49f3c
Justin Edmund <justin@jedmund.com>
2023-01-24 02:06:15 -0800

    Add type and convenience constant

---
+++ types/JobAccessory.d.ts
@@ -0,0 +1,11 @@
+interface JobAccessory {
+  id: string
+  granblue_id: string
+  job: Job
+  name: {
+    [key: string]: string
+    en: string
+    ja: string
+  }
+  rarity: number
+}
---
+++ utils/jobsWithAccessories.tsx
@@ -0,0 +1,1 @@
+export const ACCESSORY_JOB_IDS = ['683ffee8-4ea2-432d-bc30-4865020ac9f4']
