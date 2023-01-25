dca5a541a357b22ec0ab7104a69654a129b8993e
Justin Edmund <justin@jedmund.com>
2023-01-24 21:22:49 -0800

    Refined base toast styles

--- components/Toast/index.tsx
+++ components/Toast/index.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames'
 import * as ToastPrimitive from '@radix-ui/react-toast'
 import './index.scss'
 
-interface Props {
+interface Props extends ToastPrimitive.ToastProps {
   className?: string
   title?: string
   content: string
@@ -22,16 +22,24 @@ const Toast = ({
 
   return (
     <ToastPrimitive.Root {...props} className={classes}>
-      {title && <ToastPrimitive.Title>{title}</ToastPrimitive.Title>}
-      <ToastPrimitive.Description>{content}</ToastPrimitive.Description>
+      <div className="Header">
+        {title && (
+          <ToastPrimitive.Title asChild>
+            <h3>{title}</h3>
+          </ToastPrimitive.Title>
+        )}
+        <ToastPrimitive.Close aria-label="Close">
+          <span aria-hidden>×</span>
+        </ToastPrimitive.Close>
+      </div>
+      <ToastPrimitive.Description asChild>
+        <p>{content}</p>
+      </ToastPrimitive.Description>
       {children && (
         <ToastPrimitive.Action asChild altText={content}>
           {children}
         </ToastPrimitive.Action>
       )}
-      <ToastPrimitive.Close aria-label="Close">
-        <span aria-hidden>×</span>
-      </ToastPrimitive.Close>
     </ToastPrimitive.Root>
   )
 }
