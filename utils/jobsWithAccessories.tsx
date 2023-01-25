a6bc5380077adb32e60b0317f87337d5f599a9ec
Justin Edmund <justin@jedmund.com>
2023-01-24 19:20:41 -0800

    Add Manadiver to accessory job IDs

--- utils/jobsWithAccessories.tsx
+++ utils/jobsWithAccessories.tsx
@@ -1,1 +1,4 @@
-export const ACCESSORY_JOB_IDS = ['683ffee8-4ea2-432d-bc30-4865020ac9f4']
+export const ACCESSORY_JOB_IDS = [
+  '683ffee8-4ea2-432d-bc30-4865020ac9f4',
+  'a5d6fca3-5649-4e12-a6db-5fcec49150ee',
+]
