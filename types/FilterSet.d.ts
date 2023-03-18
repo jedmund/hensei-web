0b861146e401d0af3d4ee4361162c2ab87fb53a7
Justin Edmund <justin@jedmund.com>
2023-03-18 13:54:23 -0700

    Create FilterSet.d.ts

---
+++ types/FilterSet.d.ts
@@ -0,0 +1,16 @@
+interface FilterSet {
+  element?: number
+  raidSlug?: string
+  recency?: number
+  full_auto?: boolean
+  auto_guard?: boolean
+  charge_attack?: boolean
+  characters_count?: number
+  weapons_count?: number
+  summons_count?: number
+  button_count?: number
+  turn_count?: number
+  name_quality?: boolean
+  user_quality?: boolean
+  original?: boolean
+}
