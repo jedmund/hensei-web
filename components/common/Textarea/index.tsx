bb81a5405540bf8b540e00683a2951c4d3edd174
Justin Edmund <justin@jedmund.com>
2023-06-30 22:17:34 -0700

    Added new Textarea component
    This is a content editable div to prepare for when we add tagging and formatting

---
+++ components/common/Textarea/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import styles from './index.module.scss'
+
+interface Props extends React.ComponentProps<'div'> {
+  bound?: boolean
+  placeholder?: string
+  value?: string
+}
+
+const Textarea = React.forwardRef<HTMLDivElement, Props>(function textarea(
+  { bound, value: initialValue, ...props }: Props,
+  forwardedRef
+) {
+  // State
+  const [value, setValue] = useState(initialValue || '')
+
+  // Classes
+  const classes = classNames(
+    {
+      [styles.textarea]: true,
+      [styles.bound]: bound,
+    },
+    props.className?.split(' ').map((className) => styles[className])
+  )
+  // Methods: Rendering
+  return (
+    <div
+      {...props}
+      contentEditable={true}
+      suppressContentEditableWarning={true}
+      className={classes}
+      ref={forwardedRef}
+    >
+      {value}
+    </div>
+  )
+})
+
+export default Textarea
