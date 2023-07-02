a9aa498ab6c896149c5e9087d80052112692981b
Justin Edmund <justin@jedmund.com>
2023-07-02 02:41:07 -0700

    Add permissive filter set
    This is the default filter set on user profiles and the saved teams page

--- utils/defaultFilters.tsx
+++ utils/defaultFilters.tsx
@@ -4,8 +4,8 @@ const DEFAULT_CHARGE_ATTACK = -1
 const DEFAULT_MIN_CHARACTERS = 2
 const DEFAULT_MIN_WEAPONS = 5
 const DEFAULT_MIN_SUMMONS = 2
-const DEFAULT_NAME_QUALITY = false
-const DEFAULT_USER_QUALITY = false
+const DEFAULT_NAME_QUALITY = true
+const DEFAULT_USER_QUALITY = true
 const DEFAULT_ORIGINAL_ONLY = false
 
 export const defaultFilterset: FilterSet = {
@@ -18,4 +18,16 @@ export const defaultFilterset: FilterSet = {
   name_quality: DEFAULT_NAME_QUALITY,
   user_quality: DEFAULT_USER_QUALITY,
   original: DEFAULT_ORIGINAL_ONLY,
+}
+
+export const permissiveFilterset: FilterSet = {
+  full_auto: -1,
+  auto_guard: -1,
+  charge_attack: -1,
+  characters_count: 0,
+  weapons_count: 0,
+  summons_count: 0,
+  name_quality: false,
+  user_quality: false,
+  original: false,
 }
