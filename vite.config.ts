d3725b9595a9daba64ee45999abb1e9059bb2af5
Justin Edmund <justin@jedmund.com>
2025-09-11 03:54:56 -0700

    Enable SVG loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			$src: fileURLToPath(new URL('./src', import.meta.url))
+		}
+	},
+	assetsInclude: ['**/*.svg'],
 	plugins: [
 		sveltekit(),
 		paraglideVitePlugin({
