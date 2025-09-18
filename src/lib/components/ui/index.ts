a642cdd85748d710360512f2bdea7b6d2f4e5802
Justin Edmund <justin@jedmund.com>
2025-09-17 22:16:10 -0700

    fix: linter fixes

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -1,9 +1,9 @@
 // UI Components barrel export
-export { default as Input } from './input/Input.svelte';
-export { default as Select } from './select/Select.svelte';
 export { default as Button } from './Button.svelte'
+export { default as Input } from './Input.svelte'
+export { default as Select } from './Select.svelte'
 
 // Re-export existing UI components
-export { default as Dialog } from './Dialog.svelte';
-export { default as Tooltip } from './Tooltip.svelte';
-export { default as ContextMenu } from './ContextMenu.svelte';
+export { default as Dialog } from './Dialog.svelte'
+export { default as Tooltip } from './Tooltip.svelte'
+export { default as ContextMenu } from './ContextMenu.svelte'
