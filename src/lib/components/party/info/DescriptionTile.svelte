d39079c8145503a4c9a64acd46c8427d9089e273
Justin Edmund <justin@jedmund.com>
2025-12-21 02:56:49 -0800

    add party info grid components

---
+++ src/lib/components/party/info/DescriptionTile.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import InfoTile from './InfoTile.svelte'
+	import DescriptionRenderer from '$lib/components/DescriptionRenderer.svelte'
+
+	interface Props {
+		description?: string
+		onOpen: () => void
+	}
+
+	let { description, onOpen }: Props = $props()
+</script>
+
+<InfoTile label="Description" clickable onclick={onOpen} class="description-tile">
+	{#if description}
+		<DescriptionRenderer content={description} truncate={true} maxLines={4} />
+	{:else}
+		<span class="empty-state">No description</span>
+	{/if}
+</InfoTile>
+
+<style lang="scss">
+	@use '$src/themes/typography' as *;
+
+	.empty-state {
+		font-size: $font-regular;
+		color: var(--text-tertiary);
+		font-style: italic;
+	}
+</style>
