28370f03f0f2aa71ae8a1e4af85f3806993a3cf3
Justin Edmund <justin@jedmund.com>
2026-03-16 12:25:33 -0700

    pass color explicitly to icon so it renders in toast context

--- src/lib/components/ui/FavoriteToast.svelte
+++ src/lib/components/ui/FavoriteToast.svelte
@@ -15,7 +15,7 @@
 <div class="favorite-toast">
 	<div class="content">
 		<span class="toast-icon">
-			<Icon name={icon} size={14} />
+			<Icon name={icon} size={14} color="var(--toast-text)" />
 		</span>
 		<p class="message">{message}</p>
 	</div>
