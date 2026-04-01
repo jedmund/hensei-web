912922279216e47cee4ef48975f5f763b1746095
Justin Edmund <justin@jedmund.com>
2026-03-31 21:00:14 -0400

    add CharacterSeriesCell and CharacterSeasonCell for database grid

---
+++ src/lib/components/database/cells/CharacterSeasonCell.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Cell } from 'wx-svelte-grid'
+	import CharacterTag from '$lib/components/tags/CharacterTag.svelte'
+
+	const { row }: Cell = $props()
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- wx-svelte-grid IRow is untyped
+	const character = row as any
+</script>
+
+<div class="season-cell">
+	<CharacterTag {character} type="season" />
+</div>
+
+<style lang="scss">
+	.season-cell {
+		display: flex;
+		align-items: center;
+		height: 100%;
+	}
+</style>
